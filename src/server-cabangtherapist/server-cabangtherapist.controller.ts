c2ecdc2a0ede434b3ae4f0b3b0647853efc6e3ad
drewjya <andre.wijaya004@binus.ac.id>
2024-09-05 10:36:37 +0700

    update

--- src/server-cabangtherapist/server-cabangtherapist.controller.ts
+++ src/server-cabangtherapist/server-cabangtherapist.controller.ts
@@ -1,7 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Param,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { AccessTokenGuard } from 'src/common/access-token.guard';
+import {
+  bad_request,
+  getUserFromReq,
+} from 'src/server-order/server-order.util';
 import { ServerCabangtherapistService } from './server-cabangtherapist.service';
 
 @Controller('server/cabangtherapist')
 export class ServerCabangtherapistController {
   constructor(private readonly service: ServerCabangtherapistService) {}
+
+  @UseGuards(AccessTokenGuard)
+  @Post(':id')
+  addTherapistCabang(
+    @Req() req: Request,
+    @Param('id') therapistId: string,
+    @Query('cabang') cabang?: string,
+  ) {
+    const user = getUserFromReq(req);
+    if (!+therapistId) {
+      throw bad_request;
+    }
+    return this.service.addTherapistCabang({
+      admin: user,
+      therapistId: +therapistId,
+      cabangId: +cabang ? +cabang : undefined,
+    });
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Delete(':id')
+  removeTherapistCabang(
+    @Req() req: Request,
+    @Param('id') therapistId: string,
+    @Query('cabang') cabang?: string,
+  ) {
+    const user = getUserFromReq(req);
+    if (!+therapistId) {
+      throw bad_request;
+    }
+    return this.service.removeTherapistCabang({
+      admin: user,
+      therapistId: +therapistId,
+      cabangId: +cabang ? +cabang : undefined,
+    });
+  }
 }
--- src/server-cabangtherapist/server-cabangtherapist.service.ts
+++ src/server-cabangtherapist/server-cabangtherapist.service.ts
@@ -1,7 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { checkUserAdmin, not_found } from 'src/server-order/server-order.util';
+import { ApiException } from 'src/utils/exception/api.exception';
+import { CurrUser } from 'src/utils/types/server.types';
 
 @Injectable()
 export class ServerCabangtherapistService {
   constructor(private prisma: PrismaService) {}
+
+  async addTherapistCabang({
+    cabangId,
+    admin,
+    therapistId,
+  }: {
+    cabangId?: number;
+    admin: CurrUser;
+    therapistId: number;
+  }) {
+    const data = await checkUserAdmin({
+      prisma: this.prisma,
+      role: ['ADMIN', 'SUPERADMIN'],
+      userId: +admin.id,
+    });
+    if (!cabangId && !data?.adminCabang) {
+      throw not_found;
+    }
+    const cabangID = cabangId ?? data.adminCabang.id;
+
+    const therapist = await this.prisma.therapist.findFirst({
+      where: {
+        id: therapistId,
+      },
+      select: {
+        cabang: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    if (!therapist) {
+      throw not_found;
+    }
+    if (therapist.cabang) {
+      throw new ApiException({
+        data: 'therapist_sudah_di_cabang_lain',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+    await this.prisma.therapist.update({
+      where: {
+        id: therapistId,
+      },
+      data: {
+        cabang: {
+          connect: {
+            id: cabangID,
+          },
+        },
+      },
+    });
+    return true;
+  }
+
+  async removeTherapistCabang({
+    cabangId,
+    admin,
+    therapistId,
+  }: {
+    cabangId?: number;
+    admin: CurrUser;
+    therapistId: number;
+  }) {
+    const data = await checkUserAdmin({
+      prisma: this.prisma,
+      role: ['ADMIN', 'SUPERADMIN'],
+      userId: +admin.id,
+    });
+    if (!cabangId && !data?.adminCabang) {
+      throw not_found;
+    }
+    const cabangID = cabangId ?? data.adminCabang.id;
+
+    const therapist = await this.prisma.therapist.findFirst({
+      where: {
+        id: therapistId,
+        cabang: {
+          id: cabangID,
+        },
+      },
+      select: {
+        cabang: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    if (!therapist) {
+      throw not_found;
+    }
+
+    await this.prisma.therapist.update({
+      where: {
+        id: therapistId,
+      },
+      data: {
+        cabang: {
+          disconnect: {
+            id: cabangID,
+          },
+        },
+      },
+    });
+    return true;
+  }
 }
